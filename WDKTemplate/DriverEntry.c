efe4d6b4c8c0e9ec3f9a8430bef2aba41dc74d1f
xuq <xuq@mail.com>
2022-06-12 18:17:27 +0800

    update

---
+++ WDKTemplate/DriverEntry.c
@@ -0,0 +1,14 @@
+#include <ntddk.h> 
+ 
+VOID DriverUnload(PDRIVER_OBJECT driver) 
+{ 
+ DbgPrint("first: Our driver is unloading…\r\n"); 
+} 
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT driver, PUNICODE_STRING reg_path) 
+{ 
+ DbgPrint("first: Hello, my salary!"); 
+ 
+ driver->DriverUnload = DriverUnload; 
+ return STATUS_SUCCESS; 
+}
